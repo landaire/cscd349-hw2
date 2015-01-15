7215ffb330b755bfcaa3258b0043f2a38e781832
Lander Brandt <landerbrandt@gmail.com>
2015-01-15 11:09:39 -0800

    Add some docs

--- src/sauron/EyeOfSauron.java
+++ src/sauron/EyeOfSauron.java
@@ -8,6 +8,10 @@ import java.util.Observer;
  * Created by Lander Brandt on 1/13/15.
  */
 public class EyeOfSauron extends Observable {
+    /**
+     * Sets the enemies that the eye sees, and notifies its minions
+     * @param enemies array of enemy groups
+     */
     public void setEnemies(SauronEnemy[] enemies) {
         setChanged();
         notifyObservers(enemies);
